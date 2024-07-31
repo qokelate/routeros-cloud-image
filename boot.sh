10915a2f5a8adf7d88fc26319e143ce8604ee78b
sma11case <udf.q@qq.com>
2024-07-31 15:00:44 +0800

    v1

---
+++ boot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+curl -LORk 'https://github.com/qokelate/routeros-cloud-image/raw/master/chr-7.15.3-auto-setup.zip'
+unzip chr-7.15.3-auto-setup.zip
+
+dd if=chr-7.15.3.img of=/dev/vda
+
+exit
+
