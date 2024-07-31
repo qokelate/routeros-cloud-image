87960af1ab417651f4e2b2d0430ebe2c1a0a0dd7
sma11case <udf.q@qq.com>
2024-07-31 15:37:48 +0800

    v1

--- aliyun.sh
+++ aliyun.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+apt update
+apt install -y unzip
+
 curl -LORk 'https://github.com/qokelate/routeros-cloud-image/raw/master/chr-7.15.3-auto-setup.zip'
 unzip chr-7.15.3-auto-setup.zip
 
